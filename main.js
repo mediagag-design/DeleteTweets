a00e04fd85c9453655fdfeb7f1b98aff53c9586e
mediagag-design <mediagag@gmail.com>
2025-12-09 15:14:10 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
+"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
-var client_uuid = "***"; // replace by X-Client-Uuid value
+var client_tid = "Tx1pTp/JIJtVu2u5JrC36JDwa3d+Nwo+j1QF81f5WKwB+k24+Mvap/aoebct1MUtZqXAp0sf9xukLBRlLnY+2X7Tw0ZpTA"; // replace by X-Client-Transaction-Id value
+var client_uuid = "8bfXX854-XXXX-4a1c-84e7-XXXXXXXXXXX"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
 var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "mediagag" // replace with your username
 var stop_signal = undefined
 var twitter_archive_content = undefined
 var twitter_archive_loading_confirmed = false
 
 var delete_options = {
+	"after_date":new Date('1900-01-01'), // year-month-day
+	"before_date":new Date('2024-01-01') // year-month-day
 	/*  from_archive: If you downloaded your archive from Twitter, set this to true.
 	    You will be prompt to upload the tweets.js file from it.
 		Advantage is that this is much more reliable and faster.
